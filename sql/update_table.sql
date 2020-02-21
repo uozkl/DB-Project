22e204698c23a66154e5e2a69cde00dbc2c6e9a8
Zekun Li <zli203@uottawa.ca>
2020-02-21 10:08:52 -0500

    update sql

--- sql/update_table.sql
+++ sql/update_table.sql
@@ -1,9 +1,9 @@
-\copy location_dimension from '.\Dimensions\dimension_location.csv' with delimiter ',' csv header;
+\copy location_dimension from 'D:\DB-Project\Dimensions\dimension_location.csv' with delimiter ',' csv header;
 
-\copy crime_dimension from '.\Dimensions\dimension_crime.csv' with delimiter ',' csv header;
+\copy crime_dimension from 'D:\DB-Project\Dimensions\dimension_crime.csv' with delimiter ',' csv header;
 
-\copy date_dimension from '.\Dimensions\dimension_date.csv' with delimiter ',' csv header;
+\copy date_dimension from 'D:\DB-Project\Dimensions\dimension_date.csv' with delimiter ',' csv header;
 
-\copy event_dimension from '.\Dimensions\dimension_event.csv' with delimiter ',' csv header;
+\copy event_dimension from 'D:\DB-Project\Dimensions\dimension_event.csv' with delimiter ',' csv header;
 
-\copy crime_fact from '.\Dimensions\dimension_fact.csv' with delimiter ',' csv header;
+\copy crime_fact from 'D:\DB-Project\Dimensions\dimension_fact.csv' with delimiter ',' csv header;
